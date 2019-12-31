71f0d1897d942f7527629e830168d7f1024cfc98
Matt Lewis <matt@mplewis.com>
2019-12-30 18:53:55 -0700

    feat: working router

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,34 +2,20 @@
   <b-container>
     <b-row>
       <b-col>
-        <h1>{{ kind }}</h1>
-      </b-col>
-      <b-col cols="2">
-        <p>{{ text }}</p>
+        <p>nav</p>
+        <ul>
+          <li><router-link to="/foo">Foo</router-link></li>
+          <li><router-link to="/bar">Bar</router-link></li>
+        </ul>
       </b-col>
     </b-row>
+    <router-view />
   </b-container>
 </template>
 
-<script lang="ts">
-type Salutation = "HELLO" | "GOODBYE";
-interface Message {
-  kind: Salutation;
-  text: string;
-}
-
-const msg = {
-  kind: "HELLO",
-  text: "Hello from typed Vue!"
-};
-
-export default {
-  data: () => msg
-};
-</script>
+<script lang="ts"></script>
 
 <style scoped lang="stylus">
-p {
-  color: blue;
-}
+p
+  color: red
 </style>
--- app/javascript/packs/front_end_app.ts
+++ app/javascript/packs/front_end_app.ts
@@ -1,14 +1,27 @@
 import Vue from "vue";
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
-import App from "../app.vue";
+
+import App from '../app.vue'
+import Foo from '../foo.vue'
+import Bar from '../bar.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
+const routes = [
+  { path: "/foo", component: Foo },
+  { path: "/bar", component: Bar }
+];
+
+const router = new VueRouter({ routes });
+
 const FrontEndApp = new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
 
 export default FrontEndApp;
