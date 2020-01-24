9bc1185bf367dc5a74e315c2a9b6551bbd72e616
Matt Lewis <matt@mplewis.com>
2020-01-24 02:30:38 -0700

    feat: edge cases for subscribing existing users

--- app/controllers/subscriber_controller.rb
+++ app/controllers/subscriber_controller.rb
@@ -1,12 +1,17 @@
 class SubscriberController < ApplicationController
   def subscribe
     p = params.require(:subscriber).permit(:email, :zip)
+    existing = Subscriber.find_by email: p['email']
+
+    render plain: 'ALREADY_CONFIRMED', status: :bad_request and return if existing&.confirmed
+    render plain: '', status: :no_content and return if existing
+
     ApplicationRecord.transaction do
       s = Subscriber.create! p
       c = ConfirmationCode.create! subscriber: s
       # TODO: send confirmation email
     end
-    render plain: '', status: 204
+    render plain: '', status: :no_content
   end
 
   def confirm
@@ -17,6 +22,6 @@ class SubscriberController < ApplicationController
       c.subscriber.update! confirmed: true
       c.delete
     end
-    render plain: '', status: 204
+    render plain: '', status: :no_content
   end
 end
--- spec/requests/subscriber_spec.rb
+++ spec/requests/subscriber_spec.rb
@@ -2,21 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'SubscriberController', type: :request do
   describe 'subscribing and confirming' do
-    it 'works as expected' do
-      expect {
-        post '/subscribe', params: { subscriber: { email: 'matt@mplewis.com', zip: 80202 } }
-      }.to change { Subscriber.count }.by(1)
-      .and change { ConfirmationCode.count }.by 1
-      expect(response).to have_http_status :no_content
+    let(:subscribe!) do
+      post '/subscribe', params: { subscriber: { email: 'matt@mplewis.com', zip: 80202 } }
+    end
+    let(:confirm!) { get "/confirm?code=#{subscriber.confirmation_code.code}" }
+    let(:subscriber) { Subscriber.last }
 
-      s = Subscriber.last
-      expect(s).to have_attributes email: 'matt@mplewis.com', confirmed: false
-
-      expect {
-        get "/confirm?code=#{s.confirmation_code.code}"
-      }.to change { s.reload.confirmed }.from(false).to(true)
-      .and change { ConfirmationCode.count }.by -1
+    it 'creates the record and sends an email' do
+      expect { subscribe! }
+        .to change { Subscriber.count }.by(1)
+        .and change { ConfirmationCode.count }.by 1
       expect(response).to have_http_status :no_content
+      expect(subscriber).to have_attributes email: 'matt@mplewis.com', confirmed: false
+
+      expect { confirm! }
+        .to change { subscriber.reload.confirmed }.from(false).to(true)
+        .and change { ConfirmationCode.count }.by -1
+      expect(response).to have_http_status :no_content
+    end
+
+    context 'when the subscriber is already confirmed' do
+      before { Subscriber.create! email: 'matt@mplewis.com', zip: 80202, confirmed: true }
+
+      it 'returns the expected error and does not reconfirm them' do
+        expect { subscribe! }.to_not change { Subscriber.count }
+        expect(Subscriber.last.confirmed).to be true
+        expect(response).to have_http_status :bad_request
+      end
+    end
+
+    context 'when the subscriber has an existing confirmation code' do
+      before do
+        s = Subscriber.create! email: 'matt@mplewis.com', zip: 80202
+        ConfirmationCode.create! subscriber: s
+      end
+
+      it 'sends another email for the existing confirmation code' do
+        expect { subscribe! }.to_not change { Subscriber.count }
+        expect(response).to have_http_status :no_content
+      end
     end
   end
 end
